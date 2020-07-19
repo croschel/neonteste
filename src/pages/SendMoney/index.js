263ee9d7ed428184fad835ec4da82ee3dd970cad
croschel <croschel000@gmail.com>
2020-07-19 14:48:23 -0300

    Transfer Box is ready

--- src/pages/SendMoney/index.js
+++ src/pages/SendMoney/index.js
@@ -1,58 +1,128 @@
+/* eslint-disable prettier/prettier */
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import Background from '../../components/Background';
 import LinearGradient from 'react-native-linear-gradient';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-
+import getRealm from '~/services/realm';
 import {
   Container,
   HeaderBox,
   HeaderTitle,
   BackButton,
   ContactsList,
-  Contact,
   Avatar,
   InfoBox,
   Name,
   Phone,
 } from '../../components/LayoutList';
+import {
+  Contact,
+  TransferBox,
+  HeaderTransfer,
+  ContactBox,
+  ContactName,
+  ContactPhone,
+  FormBox,
+  Title,
+  InputValue,
+  SubmitButton,
+  SubmitText,
+} from './styles';
 
 function SendMoney() {
+  // states
+  const [contacts, setContacts] = useState([]);
+
+  // navigate
   const navigation = useNavigation();
+
+  // loadContacts
+  useEffect(() => {
+    async function loadContacts() {
+      const realm = await getRealm();
+      const response = realm.objects('Contact');
+      setContacts(response);
+    }
+    loadContacts();
+  }, []);
+
+  // function to handle with selected contact on press
+  function handleSelectedContact(contact) {
+
+  }
+
+  // stack functions
   function goBack() {
     navigation.goBack();
   }
   return (
-    <Background>
-      <HeaderBox>
+    <Background >
+      <HeaderBox >
         <BackButton onPress={goBack}>
           <Icon name="keyboard-arrow-left" size={50} color="#fff" />
         </BackButton>
         <HeaderTitle>ENVIAR DINHEIRO</HeaderTitle>
       </HeaderBox>
-      <Container>
+      <Container >
         <ContactsList>
-          <Contact
-            style={{
-              borderTopWidth: 1,
-              paddingTop: 10,
-              borderTopColor: '#085a6b',
-            }}>
+          {contacts.map((contact) => (
+            <Contact
+              key={contact.id}
+              onPress={() => handleSelectedContact(contact)}
+              style={{
+                borderTopColor: '#085a6b',
+              }}>
+              <LinearGradient
+                colors={['#053e57', '#00fcc2']}
+                style={{ borderRadius: 37, width: 76, height: 76 }}>
+                <Avatar
+                  source={{
+                    uri: `https://api.adorable.io/avatars/80/neon${contact.id}.png`,
+                  }}
+                />
+              </LinearGradient>
+              <InfoBox>
+                <Name>{contact.name}</Name>
+                <Phone>{contact.phone}</Phone>
+              </InfoBox>
+            </Contact>
+          ))}
+        </ContactsList>
+      </Container>
+      <TransferBox>
+        <HeaderTransfer>
+          <Icon name="cancel" size={50} color="#fff" />
+          <ContactBox>
             <LinearGradient
               colors={['#053e57', '#00fcc2']}
               style={{ borderRadius: 37, width: 76, height: 76 }}>
               <Avatar
-                source={{ uri: 'https://api.adorable.io/avatars/80/neon.png' }}
+                source={{
+                  uri: 'https://api.adorable.io/avatars/80/neon.png',
+                }}
               />
             </LinearGradient>
-            <InfoBox>
-              <Name>Anderson Santos</Name>
-              <Phone>(11)98456-8745</Phone>
-            </InfoBox>
-          </Contact>
-        </ContactsList>
-      </Container>
+            <ContactName>Anderson Santos</ContactName>
+            <ContactPhone>(11)98456-8745</ContactPhone>
+          </ContactBox>
+
+        </HeaderTransfer>
+        <FormBox>
+          <Title>Valor a enviar:</Title>
+          <InputValue
+            autoCorrect={false}
+            keyboardType="number-pad"
+            placeholder="R$ 0,00"
+            placeholderTextColor="#09a4ab"
+          />
+          <SubmitButton>
+            <SubmitText>ENVIAR</SubmitText>
+          </SubmitButton>
+        </FormBox>
+      </TransferBox>
+
     </Background>
   );
 }
---
+++ src/pages/SendMoney/styles.js
@@ -0,0 +1,68 @@
+import styled from 'styled-components/native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+export const Contact = styled(TouchableOpacity)`
+  flex-direction: row;
+  align-items: center;
+  margin: 0 20px;
+  border-bottom-width: 1px;
+  border-bottom-color: #085a6b;
+  margin-bottom: 10px;
+  padding-bottom: 10px;
+`;
+
+export const TransferBox = styled.SafeAreaView`
+  padding: 15px;
+  margin: 8%;
+  background: #1f576b;
+  border-radius: 18px;
+`;
+export const HeaderTransfer = styled.View`
+  flex-direction: row;
+`;
+export const ContactBox = styled.View`
+  align-items: center;
+  margin: 0 40px;
+`;
+export const ContactName = styled.Text`
+  font-size: 20px;
+  color: #fff;
+  font-weight: bold;
+`;
+export const ContactPhone = styled.Text`
+  font-size: 18px;
+  color: #fff;
+`;
+
+export const FormBox = styled.View`
+  align-items: center;
+  margin-top: 20px;
+`;
+
+export const Title = styled.Text`
+  font-size: 18px;
+  color: #fff;
+`;
+export const InputValue = styled.TextInput`
+  background: #f1f1f1;
+  width: 300px;
+  border-radius: 10px;
+  padding: 5px;
+  margin: 15px 0;
+  text-align: center;
+  font-size: 38px;
+  font-weight: bold;
+  color: #09a4ab;
+`;
+export const SubmitButton = styled(TouchableOpacity)`
+  background: #09a4ab;
+  width: 300px;
+  border-radius: 40px;
+`;
+export const SubmitText = styled.Text`
+  font-size: 22px;
+  font-weight: bold;
+  color: #fff;
+  padding: 15px;
+  text-align: center;
+`;
